5735cf585be5ae795571c7f06ed3a675fb7bd392
adam-street <adam.street.dev@gmail.com>
2022-10-04 12:05:14 -0500

    crud ops added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpService} from "./http.service";
+import {first} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'json-server-examples';
+
+  users: any = null;
+  nameInput: string = "";
+  userIdInput: string = "";
+
+  updateUserInputId: string = "";
+  updateUserInputName: string = "";
+
+  constructor(private httpService: HttpService) {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.httpService.getUsers().pipe(first()).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.users = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  createUser() {
+    const newUser = { id: new Date().getTime(), name: this.nameInput }
+    this.httpService.createUser(newUser).pipe(first()).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.getUsers();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  deleteUser() {
+    const id = parseInt(this.userIdInput);
+    this.httpService.deleteUser(id).pipe(first()).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.getUsers();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  updateUser() {
+    const updatedUser = {
+      id: parseInt(this.updateUserInputId),
+      name: this.updateUserInputName
+    }
+
+    this.httpService.updateUser(updatedUser).pipe(first()).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.getUsers();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
